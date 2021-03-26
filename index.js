08b1149a87fd44ef7487c42ebc29ef22be780ff9
Korenkova Kateryna <korenkova.kateryna@gmail.com>
2021-03-26 17:57:12 +0200

    User Profile

---
+++ index.js
@@ -0,0 +1,7 @@
+var userName= 'Kateryna';
+var userSecondName= 'Korenkova';
+var age= '33';
+var message= 'Hello Friend';
+profileMessage= userName+ ''+userSecondName+ ''+ age+''+message;
+
+console.log (userName, userSecondName, age, message);
